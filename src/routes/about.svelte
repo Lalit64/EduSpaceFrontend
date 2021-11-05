55e3214d8aea88458488c7b312205e36cac4e14b
Lalit_Yalamanchili <lalit.yalamanchili@gmail.com>
2021-11-05 09:41:57 +0200

    Commit

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,7 +6,9 @@
 	class="flex items-center flex-col justify-center dark:bg-gray-800 w-screen h-screen text-body dark:text-white"
 >
 	<Navbar about={true} home={false} />
-	<div class="flex items-center flex-col justify-center w-1/3 h-screen">
+	<div
+		class="flex items-center flex-col justify-center w-1/3 h-screen dark:bg-gray-700 bg-gray-300"
+	>
 		<h1 class="text-2xl font-bold mb-20 self-start">About</h1>
 		<p class="font-semibold lg:w-full space-y-2">
 			Hi, Welcome to EduSpace this web application was created for a hackathon with the theme of

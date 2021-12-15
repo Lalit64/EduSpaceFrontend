9140db8dc2adc5add2dce80a610e47a03ec011fb
Lalit64 <lalit.yalamanchili@gmail.com>
2021-12-15 18:52:28 +0200

    Commit

--- src/routes/action.svelte
+++ src/routes/action.svelte
@@ -4,14 +4,7 @@
 
 <main class="w-screen h-screen dark:bg-gray-800 text-body dark:text-white">
 	<Navbar action={true} />
-	<div class="banner h-2/3 w-full bg-gray-900 font-bold flex items-center">
-		<div class="container mx-auto">
-			<div class="flex justify-center flex-col">
-				<h1 class="text-8xl font-bold text-body">Take action</h1>
-				<p class="text-6xl text-accent">Against</p>
-			</div>
-		</div>
-	</div>
+	Coming Soon...
 </main>
 
 <style>
@@ -19,6 +12,5 @@
 		background-size: cover;
 		background-position: center;
 		background-repeat: no-repeat;
-		background-image: url(/graphics/windpower.jpg);
 	}
 </style>
